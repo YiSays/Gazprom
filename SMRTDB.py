84313a6c515ff346097007514c735dbc5f7c4026
YiSays <53087047+YiSays@users.noreply.github.com>
2020-12-02 17:45:18 +0000

    Git Error

--- SMRTDB.py
+++ SMRTDB.py
@@ -41,4 +41,5 @@ class Reading(Base): # Table readings for actural meter readings linked to filen
 Session = sessionmaker(bind=engine)
 
 if __name__ == '__main__':
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
